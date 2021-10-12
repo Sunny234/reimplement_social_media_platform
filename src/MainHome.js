ec5245321b879ac8e5e5dc9f0400209ce8c78435
Nathan Grove <nathangrove08@gmail.com>
2021-10-12 01:51:58 -0700

    updated some styling for easier positioning, renamed and moved around files

--- src/MainHome.js
+++ src/MainHome.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Search from './componets/Search'
 import Sidebar from './componets/Sidebar'
-import Home from './componets/Home'
+import Feed from './componets/Feed'
+import CreateTweet from './componets/CreateTweet'
 
 const MainHome = () => {
     return (
         <div className="main-container">
             <Search/>
             <Sidebar/>
-            <Home/>
+            <Feed/>
+            <CreateTweet/>
         </div>
     )
 }
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,10 +5,8 @@ const Tweet = ({user, tweet_content}) => {
     return (
         <div className="tweet-container">
             <a href="/"><img src={profile} alt={user}></img></a>
-            <div className="tweet-content">
-                <h1>{user}</h1>
-                <p>{tweet_content}</p>
-            </div>
+            <h1 className="tweet-content">{user}</h1>
+            <p className="tweet-content">{tweet_content}</p>
         </div>
     )
 }
