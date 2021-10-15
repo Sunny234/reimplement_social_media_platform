84683bd662cbc767ff903f0731796748978629a7
Oyoi <kvu052099@gmail.com>
2021-10-14 17:07:10 -0700

    Fixed LoadTimeline.py and add ed sample ouutput

--- Backend/LoadTimeline.py
+++ Backend/LoadTimeline.py
@@ -8,13 +8,19 @@ def LoadTimeline():
             f = json.dumps(userTweet._json, ensure_ascii = False, indent = 4)
             #Appends json files to a list of files, creating a timeline
             timeline.append(f)
-
+            twt = open("Tweet {}.json".format(i + 1), "w")
+            twt.write(f)
+            twt.close
+        print(timeline)
         return timeline
 
+    cursor = api.user_timeline()
+    timeline = []
+    FetchTimeline(cursor, timeline)
     
 #Required to function with AWS Lambda
 def lambda_handler(event, context):
     LoadTimeline()
 
 #For testing code on local machine
-#LoadTimeline()
+LoadTimeline()
