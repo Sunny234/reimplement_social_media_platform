45228eddf1e342169eed39eca2ea1ee923b14d70
Oyoi <kvu052099@gmail.com>
2021-10-25 03:55:58 -0700

    latest version lambda functions

--- Backend/GetPIN.py
+++ Backend/GetPIN.py
@@ -2,15 +2,16 @@ import tweepy
 import json
 import os
 
-def Retweet(tweetID):
+def GetPIN(PIN):
+    int(PIN)
+    tokens = []
     c = os.environ['API_KEY']
     d = os.environ['API_KEY_SECRET']
-    e = os.environ['ACCESS_TOKEN']
-    f = os.environ['ACCESS_TOKEN_SECRET']
-    auth = tweepy.OAuthHandler(c, d)
-    auth.set_access_token(e, f)
-    api = tweepy.API(auth, wait_on_rate_limit = True)
-    api.retweet(tweetID)
+    login = tweepy.OAuthHandler(c, d)
+    login.get_access_token(PIN)
+    tokens.append(login.access_token)
+    tokens.append(login.access_token_se)
+    return tokens
 
 def lambda_handler(event, context):
     return {
@@ -19,6 +20,6 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': 'http://localhost:3000',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
             },
-        'statusCode': Retweet(event['body']),
-        'body': json.dumps('Hello from Lambda!')
+        'statusCode': 200,
+        'body': GetPIN(event['body'])
     }
