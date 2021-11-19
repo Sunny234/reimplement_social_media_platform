e5a546c6065639964adbfe6d537416bdd936e97e
ethanr555 <007228488@coyote.csusb.edu>
2021-11-19 11:18:10 -0800

    Improved Post Tweet. Needs testing.

--- Backend/PostTweet.py
+++ Backend/PostTweet.py
@@ -2,37 +2,34 @@ import tweepy
 import json
 import os
 
+charLimit = 280
+
 def PostTweet(input):
     print(input)
     transformed_input = json.loads(input)
-    charLimit = 280
     c = os.environ['API_KEY']
     d = os.environ['API_KEY_SECRET']
-    e = transformed_input["access_token"]#os.environ['ACCESS_TOKEN'] 
-    f = transformed_input["access_token_secret"]#os.environ['ACCESS_TOKEN_SECRET']
+    e = transformed_input["access_token"]
+    f = transformed_input["access_token_secret"]
     g = transformed_input["message"]
+    isReply = transformed_input["is_reply"]
+    replyID = transformed_input["reply_ID"]
     auth = tweepy.OAuthHandler(c, d)
     auth.set_access_token(e, f)
     api = tweepy.API(auth, wait_on_rate_limit=True)
-    #rateLimitCountNumber = rateLimitCount(api, auth)
-    #print(rateLimitCountNumber)
-    
-    if (len(g) > charLimit):
-        tweet_count = ((len(g) // charLimit) + 1) if (len(g) % charLimit > 0) else (len(g) // charLimit)
-        print(tweet_count)
-        #if (rateLimitCount > tweet_count):
-        #parse tweets and send each one as a separate status.
+  
+    if (len(g) > 280):
+        tweets = tweet_parser(message)
         prev_id = 0
         tweet_info = ""
-        for i in range(tweet_count):
-            message_temp = ""
-            for j in range((i)*charLimit, charLimit*(i+1) if (len(g) > charLimit*(i+1)) else len(g)):
-                message_temp += g[j]
-            
-            if (i + 1 == 1):
-                post_tweet = api.update_status(message_temp)
+        for i in range(len(tweets)):   
+            if (i == 0):
+                if (isReply):
+                    post_tweet = api.update_status(status=tweets[i], in_reply_to_status_id=replyID, auto_populate_reply_metadata=True)
+                else:
+                    post_tweet = api.update_status(status=tweets[i])
             else:
-                post_tweet = api.update_status(status=message_temp, in_reply_to_status_id=prev_id, auto_populate_reply_metadata=True)
+                post_tweet = api.update_status(status=tweets[i], in_reply_to_status_id=prev_id, auto_populate_reply_metadata=True)
                 
             tweet_info = json.dumps(post_tweet._json, ensure_ascii = False, indent = 4)
             new_tweet_info = json.loads(tweet_info)
@@ -45,6 +42,38 @@ def PostTweet(input):
         tweet_info = json.dumps(post_tweet._json, ensure_ascii = False, indent = 4)
         return json.dumps({"message":"Single-tweet success"}) #tweet_info
 
+#tweet_parser parses the tweets and splits up a long messages into a formatted thread of tweets.
+#reutrns list messages, which contains a tweet messgae within 280 characters
+def tweet_parser(message):
+    messages = []
+    i = 0
+    word_temp = {"pos": 0, "word":""}
+    while i < len(message):
+        message_temp = ""
+        while (len(message_temp) + len(word_temp) < charLimit) and i < len(message):
+            message_temp += word_temp["word"] + " "
+            i = word_temp["pos"]
+            print(i)
+            word_temp = word_parser(i, message)
+        messages.append(message_temp)
+    for j in range(len(messages)):
+        messages[j] = messages[j] + "[" + str((j + 1)) + "/" +  str(len(messages)) + "]"
+    return messages
+
+#word_parser will go through the provided string, starting at position i, and find a word whitespaced by spaces. The function returns the new position and word.
+def word_parser(i, message):
+    word_temp = ""
+    j = i
+    k = 0
+    while (j < len(message) and message[j] == ' '):
+        j += 1
+    while (k < 272 and j < len(message) and message[j] != " "):
+        word_temp += message[j]
+        j += 1
+        k += 1
+    j += 1
+    result = dict(pos=j, word=word_temp)
+    return result
 
 def lambda_handler(event, context):
      return {
